0ce421b3f17ad18e50b74579b1b27734345a0639
Rodrigo Herrainz <rherrainz@gmail.com>
2025-04-24 14:46:45 -0300

    first commit

---
+++ src/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.username + " (" + self.email + ")"
+    
